82261263fa9fabac688d4808c299e502cfd66f04
Zgjim Neziraj <=>
2024-04-01 16:05:26 +0200

    Changed the name of the button in register page from Submit to Register

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -66,7 +66,7 @@
                 <div class="mb-6">
                     <button type="submit"
                             class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
-                        Submit
+                        Register
                     </button>
                 </div>
                 {{--                down the form errors--}}
